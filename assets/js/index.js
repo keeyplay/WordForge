cec5e56b97ce1f50c85dbea05110a34e7c558cdb
keeeyplay <keyplay075@gmail.com>
2026-02-05 22:35:19 +0300

    Day 3, links
    +links
    +started create information

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,13 +12,30 @@ document.getElementById('btn_start').addEventListener('click', function() {
         // document.body.style.display = "block";
         
         const newContent = `
-            <div id="new-content">
-                <div id="logo-name"><h1>WordForge</h1></div>
-            </div>
-            <div>
-                <!-- <h1 style="display: flex; justigy-content: end;">EN</h1> -->
+            <div id="all-content">
+                <div id="new-content">
+                    <div id="abtsaas">
+                        <h1>About WordForge</h1>
+                    </div>
+                    <div id="logo-name"><h1>WordForge</h1></div>
+                    <div id="abtme">
+                        <h1>About me</h1>
+                    </div>
+                </div>
+                <div id="links">
+                    <div id="github">
+                        <a href="https://github.com/keeyplay/WordForge" target="_blank"><img src="assets/pic/github.png" style="max-width: 50px"></a>
+                    </div>
+                    <div id="twitter">
+                        <a href="https://x.com/keeyplay" target="_blank"><img src="assets/pic/x.jpg" style="max-width: 50px"></a>
+                    </div>
+                    <div id="mysite">
+                        <a href="" target="_blank"><img src="assets/pic/website.png" style="max-width: 50px"></a>
+                    </div>
+                </div>
             </div>
         `;
+        document.body.style.height = "100vh";
         // getElementById("logo-name").classList.add('ani');
         document.body.innerHTML += newContent; 
     }, 1500); 
