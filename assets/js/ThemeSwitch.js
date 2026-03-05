0a080da961d4eb29bae6fe1db9da2ace612b9b3b
keeeyplay <keyplay075@gmail.com>
2026-03-05 21:59:03 +0300

    Day 31: Theme switch button work
    +add switch system button
    +add smooth animations
    +add new tasks
    +add image of dark theme button

--- assets/js/ThemeSwitch.js
+++ assets/js/ThemeSwitch.js
@@ -1,9 +1,13 @@
-let ThemeMode = "light";
+let ThemeMode = localStorage.getItem("ThemeMode") || "light";
 
 document.getElementById("ThemeSwitcher").addEventListener('click', function() {
-    if(ThemeMode = "light") {
+    if(ThemeMode == "light") {
         ThemeMode = "dark";
-    } else if(ThemeMode = "dark") {
+        document.getElementById("switchTheme").src = "assets/pic/dark.png"
+        localStorage.setItem("ThemeMode", "dark");
+    } else if(ThemeMode == "dark") {
         ThemeMode = "light";
+        document.getElementById("switchTheme").src = "assets/pic/sun.png"
+        localStorage.setItem("ThemeMode", "light");
     }
 });
