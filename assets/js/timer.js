c21af65eb08ef5c4e36bd23b0a2df55867f00840
keeeyplay <keyplay075@gmail.com>
2026-05-26 23:41:57 +0300

    Day 113: add timer system - 15 min practice mode
    + added working 15-minute timer
    + start / stop
    + congratulation message when user completes 15 min
    + timer can be stopped and restarted anytime
    + add new cool task

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,7 +1,45 @@
-let timer = document.getElementById('time');
-let start = document.getElementById('start-timer');
+const timer = document.getElementById('time');
+const start = document.getElementById('start-timer');
+const startTxt = document.getElementById('starttxt');
 
-function timer() {
-    let mins = 15;
-    let secs = 0;
+let timerIn = null; 
+let mins = 15;
+let secs = 0;
+
+start.addEventListener('click', toggleTimer);
+
+function toggleTimer() {
+    if (timerIn !== null) {
+        stopTimer();
+        return;
+    }
+
+
+    startTxt.innerText = 'Stop';
+
+    timerIn = setInterval(() => {
+        if (secs <= 0) {
+            if (mins > 0) {
+                mins--;
+                secs = 59;
+            } else {
+                alert("15 minutes have passed");
+                stopTimer();
+                return;
+            }
+        } else {
+            secs--;
+        }
+
+        timer.innerText = `${mins}:${secs.toString().padStart(2, '0')}`;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerIn);
+    timerIn = null;
+    mins = 15;
+    secs = 0;
+    timer.innerText = `15:00`;
+    startTxt.innerText = 'Start';
 }
