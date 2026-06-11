22bdb25e49fc443013cad508aceb8f7f820c9fbf
keeeyplay <keyplay075@gmail.com>
2026-06-11 23:45:10 +0300

    Day 129: refactor tooltips — now reusable + achievements planned
    + tooltips function now accept title and text as arguments
    + can create any hint without duplicating code
    + started planning achievements system

--- assets/js/tips.js
+++ assets/js/tips.js
@@ -1,12 +1,19 @@
 const notification = document.getElementById('notification');
 const closeBtn = document.getElementById('closeNotificationBtn');
 
-function showNotification() {
-    if(localStorage.getItem('Profile') && cards.length === 0) {
-        notification.classList.add('show');
-    }
+//UI vars
+const TipTitleUI = document.getElementById('TipTitle');
+const TipTextUI = document.getElementById('TipText');
+
+function showNotification(tiptitle, tiptext) {
+    notification.classList.add('show');
+    TipTitleUI.innerText = tiptitle;
+    TipTextUI.innerText = tiptext;
 }
-showNotification();
+if(localStorage.getItem('Profile') && cards.length === 0) {
+    showNotification("Welcome to WordForge 🃏", "Learn with flashcards. Flip, memorize, repeat. Stats, streaks, and coins inside. Add your first card to start.");    
+}
+
 
 function hideNotification() {
     notification.classList.remove('show');
