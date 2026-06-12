a370d7441870dbe671a0e69eeb63f6b6929de35f
keeeyplay <keyplay075@gmail.com>
2026-06-12 23:50:45 +0300

    Day 130: upgrade tooltip function — add type argument (notification / achievement)
    + added type argument to notification function
    + changes appearance based on type (hint vs achievement)
    + different styles for achievements

--- assets/js/tips.js
+++ assets/js/tips.js
@@ -4,8 +4,16 @@ const closeBtn = document.getElementById('closeNotificationBtn');
 //UI vars
 const TipTitleUI = document.getElementById('TipTitle');
 const TipTextUI = document.getElementById('TipText');
+const notificationIcon = document.getElementById('notification-icon');
+const notificationBox = document.getElementById('notification-box');
 
-function showNotification(tiptitle, tiptext) {
+function showNotification(tiptitle, tiptext, type = "not") {
+    if(type === "not") { //not = notification
+        notificationIcon.innerText = "ℹ️"
+    } else if(type = "ach") { //ach = achivment
+        notificationIcon.innerText = "🏆";
+        notificationBox.style.border = "5px solid yellow";
+    }
     notification.classList.add('show');
     TipTitleUI.innerText = tiptitle;
     TipTextUI.innerText = tiptext;
@@ -13,7 +21,6 @@ function showNotification(tiptitle, tiptext) {
 if(localStorage.getItem('Profile') && cards.length === 0) {
     showNotification("Welcome to WordForge 🃏", "Learn with flashcards. Flip, memorize, repeat. Stats, streaks, and coins inside. Add your first card to start.");    
 }
-
 
 function hideNotification() {
     notification.classList.remove('show');
