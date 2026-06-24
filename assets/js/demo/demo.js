d94d46a7943b53bd6fed2978ecde2c8451767a00
keeeyplay <keyplay075@gmail.com>
2026-06-24 23:46:21 +0300

    Day 142: landing page redesign + cosmic animations + dark theme default
    + added 500 floating particles with variable speed - looks cosmic
    + fixed multiple bugs across the landing
    + added new info blocks
    + implemented parallax effect for interactive animation of cards
    + added infinite card flip animation to show how WordForge works
    + dark theme is now default
    + update demo.png
    + added new tasks

--- assets/js/demo/demo.js
+++ assets/js/demo/demo.js
@@ -1,24 +1,29 @@
-// THEME
-const toggle = document.getElementById("themeToggle");
-
-toggle.onclick = () => {
-    document.body.classList.toggle("dark");
-};
-
 // PARTICLES
 const particles = document.getElementById("particles");
+// THEME
+const toggle = document.getElementById("themeToggle");
+document.body.classList.toggle("dark");
+toggle.onclick = () => {
+    document.body.classList.toggle("dark");
 
-for (let i = 0; i < 50; i++) {
+    const childDivs = particles.querySelectorAll('div');
+    childDivs.forEach((div)=> {
+        if(document.body.classList.contains('dark')) div.style.background = "white";
+        else div.style.background = "black";
+    });
+};
+
+for (let i = 0; i < 500; i++) {
     const p = document.createElement("div");
     p.classList.add("particle");
 
     p.style.left = Math.random() * 100 + "%";
-    p.style.animationDuration = 8 + Math.random() * 8 + "s";
+    p.style.animationDuration = 3 + Math.random() * 8 + "s";
     p.style.animationDelay = Math.random() * 5 + "s";
-    if(document.body.background === "white") {
-        p.style.background = "black";
-    } else {
+    if(document.body.classList.contains('dark')) {
         p.style.background = "white";
+    } else {
+        p.style.background = "black";
     }
     particles.appendChild(p);
 }
@@ -46,3 +51,33 @@ document.querySelectorAll(".fade").forEach(el => {
     observer.observe(el);
 });
 
+
+//mouse paralax
+const card = document.querySelector(".phone");
+let mouseX = 0;
+let mouseY = 0;
+
+let currentX = 0;
+let currentY = 0;
+
+document.addEventListener("mousemove",(e)=>{
+    const rect = card.getBoundingClientRect();
+
+    mouseX = (e.clientX - rect.left - rect.width/2)/15;
+    mouseY = (e.clientY - rect.top - rect.height/2)/15;
+});
+
+function animate(){
+
+    currentX += (mouseX-currentX)*0.1;
+    currentY += (mouseY-currentY)*0.1;
+
+    card.style.transform = `
+        rotateY(${currentX}deg)
+        rotateX(${-currentY}deg)
+    `;
+
+    requestAnimationFrame(animate);
+}
+
+animate();
