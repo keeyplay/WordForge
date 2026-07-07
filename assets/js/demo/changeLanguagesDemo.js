ae1a2fa059e3cbe614f9dadf9224ceb526f3c0e1
keeeyplay <keyplay075@gmail.com>
2026-07-07 13:00:00 +0300

    Day 155: language dropdown toggle added
    + dropdown opens and closes on click
    + basic toggle functionality working

--- assets/js/demo/changeLanguagesDemo.js
+++ assets/js/demo/changeLanguagesDemo.js
@@ -1,2 +1,17 @@
 const buttLang = document.getElementById('butt-lang');
 const dropdown = document.getElementById('dropdown-lang');
+
+function openDropdown() {
+    dropdown.style.display = "block";
+}
+
+function closeDropdown() {
+    dropdown.style.display = "none";
+}
+
+function Dropdown() {
+    if(dropdown.style.display === "none") openDropdown();
+    else closeDropdown();
+}
+
+buttLang.addEventListener('click', Dropdown);
