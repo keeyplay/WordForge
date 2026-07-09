b2d02c7d55ed06eb3e4d756d9cea21b25d64feb3
keeeyplay <keyplay075@gmail.com>
2026-07-09 10:00:00 +0300

    Day 157: full i18n system for landing page
    + added unique ids to all translatable elements
    + created JSON file with translations for 5 languages
    + built translation function for entire landing
    + all 5 languages working fast and smooth
    + JSON parses in seconds
    + saves selected language to localStorage
    + dropdown closes nicely after selection

--- assets/js/demo/changeLanguagesDemo.js
+++ assets/js/demo/changeLanguagesDemo.js
@@ -15,3 +15,35 @@ function Dropdown() {
 }
 
 buttLang.addEventListener('click', Dropdown);
+
+//translate -------------------------------------------------
+let UserLang = navigator.language.split('-')[0];
+let switchLanCount = localStorage.getItem("language");
+if(!switchLanCount) {
+    if(UserLang !== "en" && UserLang !== "de" && UserLang !== "ru" && UserLang !== "pl" && UserLang !== "sr" && UserLang !== "ja") {
+        switchLanCount = "en";
+    } else {
+        switchLanCount = UserLang;
+    }
+}
+let Translate;
+
+//get languages.json
+async function loadLanguages() {
+    const response = await fetch('../../languages-demo.json');
+    Translate = await response.json();
+
+    // document.getElementById("change_language").innerText = switchLanCount === "en" ? "🇬🇧" : "🇷🇺";
+    switchLanguageForModal(switchLanCount);
+
+    //if(localStorage.getItem("language") === "['ru']")
+}
+loadLanguages();
+
+function switchLanguageForModal(switchLan) {
+    for(const [key, value] of Object.entries(Translate)) {
+        document.getElementById(key).innerText = value[switchLan];
+    }
+    localStorage.setItem("language", switchLan);
+    closeDropdown();
+}
