971de98986966152cda0ec6eb00d33339ca78312
keeeyplay <keyplay075@gmail.com>
2026-08-29 23:42:29 +0300

    Day 207: automatic language detection for Chinese users
    + added geolocation-based language detection
    + Chinese language now auto-selects for users from China
    + improved user experience for international visitors

--- assets/js/changeLanguagesTrainer.js
+++ assets/js/changeLanguagesTrainer.js
@@ -2,7 +2,7 @@
 let UserLang = navigator.language.split('-')[0];
 let switchLanCount = localStorage.getItem("language");
 if(!switchLanCount) {
-    if(UserLang !== "en" && UserLang !== "de" && UserLang !== "ru" && UserLang !== "pl" && UserLang !== "sr" && UserLang !== "ja" && UserLang !== "ka" && UserLang !== "el" && UserLang !== "es" && UserLang !== "pt" && UserLang !== "fi") {
+    if(UserLang !== "en" && UserLang !== "de" && UserLang !== "ru" && UserLang !== "pl" && UserLang !== "sr" && UserLang !== "ja" && UserLang !== "ka" && UserLang !== "el" && UserLang !== "es" && UserLang !== "pt" && UserLang !== "fi" && UserLang !== "zh") {
         switchLanCount = "en";
     } else {
         switchLanCount = UserLang;
